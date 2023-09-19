f3b44b924f3eac2dcdef6cf5f17ec054f5c5b31f
Dhananjayan <dhananjayan.s.n@doodleblue.in>
2023-09-19 11:46:35 +0530

    Base path Updates

--- dist/assets/Header-f7f59e3d.js
+++
@@ -1,1 +0,0 @@
-import{j as e,c as n}from"./index-7c61f515.js";const r="_container_7qe11_14",t="_header_7qe11_21",a={container:r,header:t};function s(){return e.jsx("div",{children:e.jsx("div",{className:n(a.header),children:e.jsx("center",{children:e.jsx("h3",{children:"Central Kitchen - Inventory management"})})})})}export{s as default};
